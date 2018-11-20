63c0e340c3b2c302605c3931ddeaf09bcc5acc96
user01 <user01@see-l12074.leeds.ac.uk>
2018-11-20 15:15:07 +0000

    k

---
+++ python_tuesday.py
@@ -0,0 +1,27 @@
+course= "python"
+rating= 4
+
+#Compute the hypotenuse using the Pythagoras theorem
+
+b=5
+c=11
+
+b=5
+c=11
+
+a=((b**2 + c**2))**(1/2)
+
+print(a)
+
+round(a,2) #round a to two decimals
+
+print(a)
+
+#display the result
+
+print(str(round(a,2)) + " squared equals" + " "+ str(b) +" "+  "squared" + "+"+str(c) + " "+ "squared")
+
+##############################
+
+
+
